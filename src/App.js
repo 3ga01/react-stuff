9aa9bac37a565460f0274a15e93a9b626728956d
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-09-18 16:46:06 +0000

    pages restructured

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Landing from "./pages/Landing/Landing";
-import "./css/formContainer.css"
+import "./css/formContainer.css";
 
 function App() {
   return (
     <div>
-      <Landing />
+      
     </div>
   );
 }
